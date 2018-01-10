d5ec2427d87d96843bb4f3053ea076c6ace2bb26
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2018-01-10 15:31:04 +0800

    refactor to use strings

--- src/contracts/Quiz.sol
+++ src/contracts/Quiz.sol
@@ -40,13 +40,15 @@ contract Quiz {
         return true;
     }
 
-    function submitYourAnswers(bytes32[] myAnswers) public returns(bool) {
+    function submitYourAnswers(string a0, string a1, string a2, string a3, string a4, string a5) public returns(bool) {
         require(claimed != true);
 
-        for (uint8 i = 0; i < answers.length; i++) {
-            bytes32 hashedAnswer = keccak256(myAnswers[i]);
-            require(hashedAnswer == answers[i]);
-        }
+        require(answers[0] == keccak256(a0));
+        require(answers[1] == keccak256(a1));
+        require(answers[2] == keccak256(a2));
+        require(answers[3] == keccak256(a3));
+        require(answers[4] == keccak256(a4));
+        require(answers[5] == keccak256(a5));
 
         claimed = true;
         msg.sender.transfer(this.balance);
@@ -58,7 +60,7 @@ contract Quiz {
 
     function refund(address to) public onlyOwner {
         require(claimed != true);
-        require(now > expires + 1 hours);
+        require(now > expires + 1 days);
         
         to.transfer(this.balance);
     }
